9f09667120ab38fb95eb6bf6775e2f91bf088d56
Shuvradeep Chatterjee <shuvradeep.96@gmail.com>
2022-09-14 20:59:55 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style.css";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h2 className="header--title">MemeGenerator</h2>
+    </header>
+  );
+}
+
+
